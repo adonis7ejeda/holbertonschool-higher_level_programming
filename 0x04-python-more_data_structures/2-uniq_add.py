6f95c866e60ce34e2e91cc2977a51c999ac779c1
adonis7ejeda <adonis7ejeda@gmail.com>
2020-05-08 07:37:00 +0000

    Add author

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
     return sum(list(set(my_list)))
+'''Create by adonis7ejeda'''
--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -2,3 +2,4 @@
 def print_sorted_dictionary(a_dictionary):
     for k, v in sorted(a_dictionary.items()):
         print("{}: {}".format(k, v))
+'''Create by adonis7ejeda'''
