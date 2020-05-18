4d315e8478bcf0ba98bae89656a59e3516b62eea
adonis7ejeda <adonis7ejeda@gmail.com>
2020-05-18 17:56:43 +0000

    Task 0.2

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
+    c = 0
     try:
         for i in my_list[0:x]:
             print(i, end='')
+            c += 1
     except IndexError:
         pass
     print()
-    return x
+    return c
 '''Create by adonis7ejeda'''
