24183545464d96f5199080aaa66587344d4bb2dd
adonis7ejeda <adonis7ejeda@gmail.com>
2020-05-18 20:00:55 +0000

    Task 2

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    c = 0
+    for i in my_list[0:x]:
+        try:
+            print("{:d}".format(i), end='')
+            c += 1
+        except TypeError:
+            break
+    print()
+    return c
