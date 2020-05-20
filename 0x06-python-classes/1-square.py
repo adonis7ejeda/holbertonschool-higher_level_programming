674b833fd737403104cd79709398005aa9db3e3e
adonis7ejeda <adonis7ejeda@gmail.com>
2020-05-20 02:50:48 +0000

    Task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""This is a Square class empty"""
+
+
+class Square:
+    """This is a Square class empty"""
+
+    def __init__(self, size):
+        """This is a private instance attribute"""
+
+        self._Square__size = size
