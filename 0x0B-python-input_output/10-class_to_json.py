3cbb43e8bb9e76e6da31090de306f9e3cb783f23
adonis7ejeda <adonis7ejeda@gmail.com>
2020-06-04 03:45:30 +0000

    Task 10.1

--- 0x0B-python-input_output/10-class_to_json.py
+++ 0x0B-python-input_output/10-class_to_json.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+"""Returns the dictionary description"""
+
+
 def class_to_json(obj):
+    """Returns the dictionary description"""
     my_dic = {}
     for key in obj.__dict__:
         my_dic[key] = obj.__dict__[key]
