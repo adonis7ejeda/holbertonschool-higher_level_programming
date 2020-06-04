de4cde65332b84f5de17f71135c3bf901b3011f5
adonis7ejeda <adonis7ejeda@gmail.com>
2020-06-04 03:25:32 +0000

    Task 4

---
+++ 0x0B-python-input_output/4-append_write.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+appends a string at the end of a text file (UTF8)
+and returns the number of characters added
+"""
+
+
+def append_write(filename="", text=""):
+    """
+    appends a string at the end of a text file (UTF8)
+    and returns the number of characters added
+    """
+    with open(filename, mode="a", encoding="utf-8") as myFile:
+        charNum = myFile.write(text)
+    return charNum
