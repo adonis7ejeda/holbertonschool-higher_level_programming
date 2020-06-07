ee99e4ebc47dd1b96ee1872fcf8938ed7c8042f4
adonis7ejeda <adonis7ejeda@gmail.com>
2020-06-07 03:18:55 +0000

    Task 8

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,15 @@
+"""
+Instantiation with width and height: def __init__(self, width, height)
+"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """
+    Instantiation with width and height: def __init__(self, width, height)
+    """
+    def __init__(self, width, height):
+        super().integer_validator("width", width)
+        self.__width = width
+        super().integer_validator("height", height)
+        self.__height = height
