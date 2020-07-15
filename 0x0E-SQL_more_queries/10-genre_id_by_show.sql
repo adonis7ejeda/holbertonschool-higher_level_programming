cc57c89b1f93e19288c0480c9d7527ff966b5f84
adonis7ejeda <adonis7ejeda@gmail.com>
2020-07-15 12:12:46 -0500

    Task 10 0x0E

---
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,7 @@
+-- Script that lists all shows contained in hbtn_0d_tvshows that have at least one genre linked
+-- SQL is fun
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_show_genres RIGHT JOIN tv_shows
+ON tv_shows.id = tv_show_genres.show_id
+WHERE tv_show_genres.show_id IS NOT NULL
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
