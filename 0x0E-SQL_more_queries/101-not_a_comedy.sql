6e4fab15996ead0cb64e51d5be00c2dfc481cf84
adonis7ejeda <adonis7ejeda@gmail.com>
2020-07-17 13:49:34 -0500

    Task 18 0x0E

---
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -0,0 +1,13 @@
+--  lists all shows without the genre Comedy
+-- SQL is fun
+SELECT title
+FROM tv_shows
+WHERE title
+NOT IN
+(SELECT tv_shows.title
+FROM tv_genres INNER JOIN tv_show_genres
+ON tv_genres.id = tv_show_genres.genre_id
+INNER JOIN tv_shows
+ON tv_shows.id = tv_show_genres.show_id
+WHERE tv_genres.name = 'Comedy')
+ORDER BY title;
