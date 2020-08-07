ec19a372d560439ffc65d52f25802166b7d91458
adonis7ejeda <adonis7ejeda@gmail.com>
2020-08-07 13:45:05 -0500

    Task 2.1 0x0F

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
-    sql = "SELECT * FROM states WHERE name LIKE '"+sys.argv[4]+"' ORDER BY id"
+    sql = "SELECT * FROM states WHERE name = '"+sys.argv[4]+"' ORDER BY id"
     cur.execute(sql)
     rows = cur.fetchall()
     for row in rows:
