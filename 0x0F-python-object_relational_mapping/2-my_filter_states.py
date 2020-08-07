3c7f161fd8b506d4ba0ae6df36f1ea3e07011a32
adonis7ejeda <adonis7ejeda@gmail.com>
2020-08-07 13:41:58 -0500

    Task 2 0x0F

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Filter states by user input"""
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    """Execute query for list all states by user input"""
+    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3])
+    cur = db.cursor()
+    sql = "SELECT * FROM states WHERE name LIKE '"+sys.argv[4]+"' ORDER BY id"
+    cur.execute(sql)
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
