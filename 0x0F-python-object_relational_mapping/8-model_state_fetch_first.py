db7c56e71f799a1db8cf12a18e25117a6b1a5e0b
adonis7ejeda <adonis7ejeda@gmail.com>
2020-08-08 15:46:09 -0500

    Task 8 0x0F

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""First state"""
+import sys
+from model_state import Base, State
+
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    state = session.query(State).first()
+    if not state:
+        print("Nothing\n")
+    else:
+        print("{}: {}".format(state.id, state.name))
+    session.close()
