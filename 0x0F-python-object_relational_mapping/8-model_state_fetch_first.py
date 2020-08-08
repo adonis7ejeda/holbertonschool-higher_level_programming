c943adab0fc922351daddf509b71d1b4132ebe6f
adonis7ejeda <adonis7ejeda@gmail.com>
2020-08-08 15:51:40 -0500

    Task 8.3 0x0F

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -15,7 +15,7 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
     session = Session()
     state = session.query(State).first()
-    if state is not None:
+    if state is None:
         print("Nothing\n")
     else:
         print("{}: {}".format(state.id, state.name))
