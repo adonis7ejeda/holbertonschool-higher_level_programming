5d6c98bd266e590d68422bd790623eda92afc264
adonis7ejeda <adonis7ejeda@gmail.com>
2020-08-09 15:45:17 -0500

    Task 15 0x0F

---
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""The class definition State"""
+
+from sqlalchemy import Column, Integer, String, Sequence
+from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class State(Base):
+    """State class"""
+    __tablename__ = 'states'
+    id = Column(Integer, Sequence('state_id_seq'), primary_key=True)
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", backref="state", cascade="all, delete")
