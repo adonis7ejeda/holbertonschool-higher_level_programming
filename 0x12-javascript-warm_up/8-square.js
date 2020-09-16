b78a0e0ac62ccb562303f3dcb2a3ff80805f88db
adonis7ejeda <adonis7ejeda@gmail.com>
2020-09-15 22:32:36 -0500

    Task 8.2 0x12

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,4 +1,7 @@
 #!/usr/bin/node
+if (isNaN(process.argv[2])) {
+  console.log('Missing size');
+}
 for (let index = 0; index < process.argv[2]; index++) {
   console.log('X'.repeat(process.argv[2]));
 }
